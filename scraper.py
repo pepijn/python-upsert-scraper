d4dcf6b79c4b361b6f0a023f312ea70013d477b5
Pepijn Looije <p@epij.nl>
2016-04-03 11:50:06 +0200

    Add main function

--- scraper.py
+++ scraper.py
@@ -1,40 +1,57 @@
+from datetime import datetime
+import difflib
+import os
+import psycopg2
+
 def diff(after, before=None):
     if not before:
         return None
 
-    from difflib import context_diff
-    diff = context_diff(before[1].splitlines(True),
-                        after[1].splitlines(True),
-                        str(before[0]),
-                        str(after[0]))
+    diff = difflib.context_diff(before[1].splitlines(True),
+                                after[1].splitlines(True),
+                                str(before[0]),
+                                str(after[0]))
     return list(diff)
 
-def query(query, params=(), database_url=None):
-    from psycopg2 import connect
-    with connect(database_url) as conn:
-        with conn.cursor() as cur:
-            cur.execute(query, params)
-            return cur.fetchall()
-
-def scrape(body, timestamp=None, database_url=None):
-    if not timestamp:
-        from datetime import datetime
-        timestamp = datetime.now()
-
-    import os
+def scrape(body, timestamp=datetime.now(), database_url=None):
+    assert timestamp
     path = os.path.join(os.path.dirname(__file__), 'query.sql')
     with open(path) as f:
-        result = query(f.read(),
-                       params=(body, timestamp),
-                       database_url=database_url)
-        return diff(*result)
+        with psycopg2.connect(database_url) as conn:
+            with conn.cursor() as cur:
+                cur.execute(f.read(), (body, timestamp))
+                results = cur.fetchall()
+                return diff(*results)
 
-def message(body):
+def main():
+    database_url = os.environ['DATABASE_URL']
+    sender = os.environ['FROM']
+    sendgrid_user = os.environ['SENDGRID_USERNAME']
+    sendgrid_pass = os.environ['SENDGRID_PASSWORD']
+
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--recipients', required=True)
+    parser.add_argument('--subject', required=True)
+    args = parser.parse_args()
+
+    import sys
+    body = sys.stdin.read()
+
+    diff = scrape(body, database_url=database_url)
+
+    if not diff:
+        return
+
     import sendgrid
-    message = sendgrid.Mail()
-    message.add_to('John Doe ')
-    message.set_subject('Example')
-    message.set_text(body)
-    message.set_from('Doe John ')
+    sg = sendgrid.SendGridClient(sendgrid_user, sendgrid_pass)
+    message = sendgrid.Mail(to=args.recipients,
+                            subject=args.subject,
+                            text=''.join(diff),
+                            from_email=sender)
+    status, msg = sg.send(message)
+    if status is not 200:
+        sys.exit(1)
 
-    return message
+if __name__ == "__main__":
+    main()
